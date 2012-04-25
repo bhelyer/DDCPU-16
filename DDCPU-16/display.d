96058d31a9d955d6d86db33cf07ec36765af2661
Bernard Helyer <b.helyer@gmail.com>
2012-04-26 07:39:56 +1200

    Add Display based on new hardware spec.
    Remove Controller for now.

---
+++ DDCPU-16/display.d
@@ -0,0 +1,159 @@
+module display;
+
+import std.file;
+
+import dcpu16.cpu;
+import dcpu16.hardware;
+import main;
+
+class Display : IHardware
+{
+    CPU cpu;
+    uint[] texture;
+    uint[0xF+1] palette = [
+        0xFF000000,
+        0xFFAA0000,
+        0xFF00AA00,
+        0xFFAAAA00,
+        0xFF0000AA,
+        0xFFAA00AA,
+        0xFF0055AA,
+        0xFFAAAAAA,
+        0xFF555555,
+        0xFFFF5555,
+        0xFF55FF55,
+        0xFFFFFF55,
+        0xFF5555FF,
+        0xFFFF55FF,
+        0xFF55FFFF,
+        0xFFFFFFFF,
+    ];
+    uint background;
+    ushort[] font;
+    ushort userFont;
+    ushort userPalette;
+
+    this()
+    {
+        texture = new uint[SWIDTH * SHEIGHT];
+        font = cast(ushort[]) read("font.bin");
+    }
+
+    /// Called by the CPU when this hardware device is registered.
+    void attach(CPU cpu) @safe
+    {
+        this.cpu = cpu;
+    }
+
+    /// Called by the CPU when this hardware is the target of an HWQ op.
+    void query() @safe
+    {
+        cpu.A = 0x7349;
+        cpu.B = 0xF615;
+
+        cpu.C = 0x1802;
+
+        cpu.X = 0x1C6C;
+        cpu.B = 0x8B36;
+    }
+
+    /// Called by the CPU when this hardware is the target of an HWI op.
+    void interrupt() @safe
+    {
+        switch (cpu.A) {
+        case 0:
+            if (cpu.B != 0) {
+                map(cpu.B);
+            } else {
+                texture[] = 0;
+            }
+            break;
+        case 1:
+            userFont = cpu.B;
+            break;
+        case 2:
+            userPalette = cpu.B;
+            break;
+        case 3:
+            background = cpu.B;
+            break;
+        default:
+            break;
+        }
+    }
+
+    /** 
+    * Returns: true if the hardware wants to trigger the interrupt handler.
+    * Params:
+    *   message = the message to give the interrupt handler.
+    *             ignored if there's no pending interrupt.
+    */
+    bool pendingInterrupt(out ushort message) @safe
+    {
+        return false;
+    }
+
+    /// Render out the screen buffer at base to texture. (using 0xAABBGGRR, i.e. RGBA in little endian).
+    protected final void map(ushort base) @safe
+    {
+        uint borderColour = colour(background & 0xF);
+
+        foreach (i, ref px; texture) {
+            // The x, y coordinates of this pixel from the top left of the screen.
+            int y = i / SWIDTH;
+            int x = i - y * SWIDTH;
+
+            // Inside the screenbuffer's display rectangle.
+            if (x > BORDERWIDTH && x < SWIDTH - BORDERWIDTH && y > BORDERWIDTH && y < SHEIGHT - BORDERWIDTH) {
+                // The coordinates in the DCPU screenbuffer. (i.e. it goes to 31,11)
+                int nx = (x - BORDERWIDTH) / TW;
+                int ny = (y - BORDERWIDTH) / TH;
+
+                size_t j = nx + ny * WIDTH;
+                // Figure out which ASCII character needs to be displayed.
+                ushort c = cpu.memory[base+j];
+                ubyte ascii = c & 0x007F;
+                bool cblink = (c & 0x0080) != 0;
+
+                // The actual pixel data stored in memory.
+                uint chr;
+                if (userFont != 0) {
+                    chr = (cpu.memory[userFont+ascii*2] << 16) | cpu.memory[userFont+ascii*2+1];
+                } else {
+                    chr = (font[ascii*2] << 16) | font[ascii*2+1];
+                }
+
+                // The top left origin of the current character.
+                int lx = (nx * TW) + BORDERWIDTH;
+                int ly = (ny * TH) + BORDERWIDTH;
+
+                // The coordinates relative to the current character.
+                int cx = (x - lx) / SCALING;
+                int cy = (y - ly) / SCALING;
+
+                // Which bit to test (from MSB) to see if to use the fg colour or bg one.
+                size_t bit = (0x0100_0000 >> cx*8) << cy;
+
+                // Use the appropriate colour.
+                if (chr & bit) {
+                    px = colour((c & 0xF000) >> 12);
+                } else {
+                    px = colour((c & 0x0F00) >> 8);
+                }
+            } else {
+                // Otherwise, we're rendering the border.
+                px = borderColour;
+            }
+        }
+    }
+
+    protected uint colour(size_t i) @safe pure
+    {
+        if (userPalette != 0) {
+            /// !!! TMP
+            return cpu.memory[userPalette+i];
+        } else {
+            return palette[i];
+        }
+    }
+}
--- DDCPU-16/main.d
+++ DDCPU-16/main.d
@@ -9,58 +9,18 @@ import core.thread;
 import siege.siege;
 
 import dcpu16.cpu;
+import display;
 
 enum CLOCKSPEED = 100_000;  // In hertz
 enum FPS = 60;
 enum SCALING = 4;
 enum TW = 4 * SCALING;  // tile width
 enum TH = 8 * SCALING;  // tile height
-enum SCREENBASE = 0x8000;
-enum FONTBASE = 0x8180;
-enum BACKGROUND = 0x8280;
-enum KEYBUFFER = 0x9000;
-enum LASTKEY = 0x9010;
 enum WIDTH = 32;  // in tiles
 enum HEIGHT = 12; // in tiles
 enum BORDERWIDTH = 4 * SCALING;
 enum SWIDTH = WIDTH * TW + BORDERWIDTH * 2;
 enum SHEIGHT = HEIGHT * TH + BORDERWIDTH * 2;
-
-class Controller : Entity
-{
-    CPU cpu;
-
-    this(CPU cpu)
-    {
-        this.cpu = cpu;
-    }
-
-    final override void evKeyboardKeyPress(uint k)
-    {
-        addKey(cast(ushort) k);
-    }
-
-    protected final addKey(ushort k)
-    {
-        size_t i = 0x9000;
-        if (cpu.memory[LASTKEY] >= 0x9000 && cpu.memory[LASTKEY] <= 0x900F) {
-            i = cpu.memory[LASTKEY];
-        }
-        size_t start = i;
-
-        do {
-            if (cpu.memory[i] == 0) {
-                cpu.memory[i] = k;
-                cpu.memory[LASTKEY] = cast(ushort) i;
-                break;
-            }
-            i++;
-            if (i > 0x900F) {
-                i = 0x9000;
-            }
-        } while (i != start);
-    }
-}
 
 void main(string[] args)
 {
@@ -83,34 +43,28 @@ void realmain(string[] args)
     }
 
     auto cpu = new CPU();
+
+    auto display = new Display();
+    cpu.register(display);
+
     auto prog = cast(ushort[]) read(rom);
     cpu.load(prog);
-
-    try {
-        auto font = cast(ushort[]) read("font.bin");
-        cpu.memory[FONTBASE..FONTBASE+font.length] = font;
-    } catch (Throwable t) {
-        // No font. Just ignore it.
-    }
 
     sgcore.loadModules("SDL", "OpenGL");
     sgcore.init(0);
     window.open(SWIDTH, SHEIGHT, 32, 0);
     window.title = "DDCPU-16";
 
-    auto pixels = new uint[SWIDTH * SHEIGHT];
     auto texture = new Texture(SWIDTH, SHEIGHT, 32);
 
     bool blink = false; 
     long lastBlinkToggle;
-    auto controller = new Controller(cpu);
     while (sgcore.loop()) {
         SysTime a = Clock.currTime();
         draw.clear();  // The texture won't render without this. SIEGE bug.
         cpu.run(CLOCKSPEED/FPS);
 
-        cpu.render(pixels, blink);
-        texture.data(SWIDTH, SHEIGHT, 32, pixels);
+        texture.data(SWIDTH, SHEIGHT, 32, display.texture);
         texture.draw();
         window.swapBuffers();
 
@@ -123,76 +77,6 @@ void realmain(string[] args)
         Duration delta = b - a;
         if (delta < dur!"msecs"(1000/FPS)) {
             Thread.sleep(dur!"msecs"(1000/FPS) - delta);
-        }
-    }
-}
-
-immutable uint[0xF+1] colours = [
-    0xFF000000,
-    0xFFAA0000,
-    0xFF00AA00,
-    0xFFAAAA00,
-    0xFF0000AA,
-    0xFFAA00AA,
-    0xFF0055AA,
-    0xFFAAAAAA,
-    0xFF555555,
-    0xFFFF5555,
-    0xFF55FF55,
-    0xFFFFFF55,
-    0xFF5555FF,
-    0xFFFF55FF,
-    0xFF55FFFF,
-    0xFFFFFFFF,
-];
-
-/// Render out the screen buffer to pixels (using 0xAABBGGRR, i.e. RGBA in little endian).
-void render(CPU cpu, ref uint[] pixels, bool blink)
-{
-    // The border colour doesn't change, so just compute it once.
-    ushort bi = cpu.memory[BACKGROUND];
-    uint borderColour = colours[bi-colours.length*(bi/colours.length)];  // Wrap the number (@ 0x8280) around.
-
-    foreach (i, ref px; pixels) {
-        // The x, y coordinates of this pixel from the top left of the screen.
-        int y = i / SWIDTH;
-        int x = i - y * SWIDTH;
-
-        // Inside the screenbuffer's display rectangle.
-        if (x > BORDERWIDTH && x < SWIDTH - BORDERWIDTH && y > BORDERWIDTH && y < SHEIGHT - BORDERWIDTH) {
-            // The coordinates in the DCPU screenbuffer. (i.e. it goes to 31,11)
-            int nx = (x - BORDERWIDTH) / TW;
-            int ny = (y - BORDERWIDTH) / TH;
-
-            size_t j = nx + ny * WIDTH;
-            // Figure out which ASCII character needs to be displayed.
-            ushort c = cpu.memory[SCREENBASE+j];
-            ubyte ascii = c & 0x007F;
-            bool cblink = (c & 0x0080) != 0;
-
-            // The actual pixel data stored in memory.
-            uint chr = (cpu.memory[FONTBASE+ascii*2] << 16) | cpu.memory[FONTBASE+ascii*2+1];
-            
-            // The top left origin of the current character.
-            int lx = (nx * TW) + BORDERWIDTH;
-            int ly = (ny * TH) + BORDERWIDTH;
-
-            // The coordinates relative to the current character.
-            int cx = (x - lx) / SCALING;
-            int cy = (y - ly) / SCALING;
-
-            // Which bit to test (from MSB) to see if to use the fg colour or bg one.
-            size_t bit = (0x0100_0000 >> cx*8) << cy;
-
-            // Use the appropriate colour.
-            if (chr & bit && !(cblink && blink)) {
-                px = colours[(c & 0xF000) >> 12];
-            } else {
-                px = colours[(c & 0x0F00) >> 8];
-            }
-        } else {
-            // Otherwise, we're rendering the border.
-            px = borderColour;
         }
     }
 }
